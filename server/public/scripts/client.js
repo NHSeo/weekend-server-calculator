5d2fe73430bcec2c9cc2a79c5e18fc6865dac6c0
Noah Seo <sht6205@gmail.com>
2024-05-21 04:16:40 -0500

    Updated get and render calculation on client

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,26 @@
 console.log('client.js is sourced!');
+
+document.addEventListener('DOMContentLoaded', function() {
+    const resultHistorySection = document.querySelector('[data-testid="resultHistory"]');
+  
+    function rendCalc(calculations) {
+      resultHistorySection.innerHTML = '';
+      for (const calc of calculations) {
+        const calcText = `${calc.numOne} ${calc.operator} ${calc.numTwo} = ${calc.result}`;
+        resultHistorySection.innerHTML += `<p>${calcText}</p>`;
+      }
+    }
+  
+    function getCalc() {
+      axios.get('/calculations')
+        .then(response => {
+          rendCalc(response.data);
+        })
+        .catch(error => {
+          console.error('Error fetching calculations:', error);
+        });
+    }
+  
+    getCalc();
+  });
+  
